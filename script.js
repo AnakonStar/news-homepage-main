46d3da14766d7a411eb89052385e279552eef4aa
AnakonStar <enzoh3h7@gmail.com>
2023-06-01 23:59:37 -0300

    Just finished the README archive

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function openNav() {
     document.getElementById("sideNav").style.display = "flex";
 }
 
-/* Set the width of the side navigation to 0 */
 function closeNav() {
     document.getElementById("sideNav").style.display = "none";
 }
