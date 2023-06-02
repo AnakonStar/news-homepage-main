cf791c7144bb8de8b6391388d00c0541ac30e027
AnakonStar <enzoh3h7@gmail.com>
2023-06-01 23:11:26 -0300

    All completed
    - Mobile View
    - Desktop View
    - Drawer function
    - Header modifies when change from desktop to mobile

---
+++ script.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("sideNav").style.display = "flex";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("sideNav").style.display = "none";
+}
